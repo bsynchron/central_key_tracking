4c8a88c4cc896b206e408ba5ee8a23f13e2bd4e3
Florian Patzwall <florian.patzwall@esailors.de>
2019-12-06 10:35:51 +0000

    added css

--- views/map/index.php
+++ views/map/index.php
@@ -4,6 +4,7 @@ include("$root/controllers/SQLController.php");
 ?>
 
 <head>
+  <link rel="stylesheet" href="/src/js/style.css" />
   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
   integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
   crossorigin=""/>
