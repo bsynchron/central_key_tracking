9bf6b943dbb6249f2c8db4c05fa9ff8c620f2b90
Florian Patzwall <florian.patzwall@esailors.de>
2019-12-07 10:07:57 +0000

    added login page

--- config/routing.php
+++ config/routing.php
@@ -4,20 +4,14 @@ $request = $_SERVER['REQUEST_URI'];
 
 if(substr($request, 0, 4) == "/api"){
   file_put_contents("php://stdout", "API REQUEST\n");
-
   include "$root/api/index.php";
-
-
-
-
-
-
-
+  
 } else {
   file_put_contents("php://stdout", "NORMAL REQUEST\n");
 
   $paths = ["/","/views/index.php","CKT",
-            "/map", "/views/map/index.php", "MAP"];
+            "/map", "/views/map/index.php", "MAP",
+            "/login", "/views/user/login.php", "LOGIN"];
 
   if(substr($request, -1) == "/" and strlen($request) > 1){
     $request = substr_replace($request, "", -1);
