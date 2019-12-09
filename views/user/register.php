ef34d94446c12666817449aca25527fbeff22902
Florian Patzwall <florian.patzwall@esailors.de>
2019-12-09 13:47:38 +0000

    indeed

---
+++ views/user/register.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+
+
+
+?>
+<form onsubmit="">
+  <input type="text" value="" placeholder="Name"></input>
+  <input type="passowrd" value="" placeholder="Password"></input>
+  <input type="submit"></input>
+</form>
